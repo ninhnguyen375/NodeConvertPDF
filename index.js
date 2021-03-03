3989d4d1f653fa26fe4214cc73f63b2cd547dfed
Ninh <ninhnguyen375@gmail.com>
2021-03-03 14:44:23 +0700

    int

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const app = express();
-const libre = require('libreoffice-convert');
-const path = require('path');
+const express = require('express')
+const app = express()
+const path = require('path')
+const { execSync } = require("child_process");
+const fs = require('fs');
+const execWordPath = "C:/\"Program Files\"/LibreOffice/program/swriter.exe"
+const execExcelPath = "C:/\"Program Files\"/LibreOffice/program/scalc.exe"
 
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
@@ -11,29 +14,43 @@ app.get('/', (req, res) => {
 })
 
 app.post('/api/convert-pdf', async (req, res) => {
-  const b64string = req.body.base64
+  const base64ToConvert = req.body.base64
+  const type = req.body.type
 
-  if(b64string === undefined) {
-    res.statusCode = 400;
-    res.send({ error: "No body data" });
-    return;
+  if (base64ToConvert === undefined) {
+    res.statusCode = 400
+    res.send({ error: "No body data" })
+    return
   }
 
-  const buffer = Buffer.from(b64string, 'base64')
+  const buffer = Buffer.from(base64ToConvert, 'base64')
+  const now = Date.now().toString()
+  const filePath = path.join(__dirname, "tmp", now) 
+  const convertedPath = path.join(__dirname, "converted")
+  const convertedItem = path.join(convertedPath, now + ".pdf")
+  fs.writeFileSync(filePath, buffer)
 
-  libre.convert(buffer, '.pdf', undefined, (err, done) => {
-    if (err) {
-      res.send({
-        error: err
-      })
-    } else {
-      console.log("done");
-      res.send({
-        base64: done.toString('base64')
-      })
-    }
-  });
-});
+  let cmd = ""
 
-app.listen(3000, () => {
+  if (type === "word") {
+    cmd = execWordPath + " --headless --convert-to pdf " + filePath + " --outdir " + convertedPath
+  } else if (type === "excel") {
+    cmd = execExcelPath + " --headless --convert-to pdf " + filePath + " --outdir " + convertedPath
+  } else {
+    res.send({ error: "missing type" })
+    return
+  }
+
+  execSync(cmd);
+  
+  const bytes = fs.readFileSync(convertedItem)
+  const base64 = bytes.toString("base64")
+
+  // delete converted files
+  execSync("del /f " + convertedItem)
+  execSync("del /f " + filePath)
+
+  res.send(base64)
 })
+
+app.listen(3000, () => { })
